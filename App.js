16a2c35a88f2f20f02292b8bcd01cc6011bc2fc7
Kamo <kamo.aghbalyan@gmail.com>
2024-04-22 13:35:54 +0400

    Home screen

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View, Text} from 'react-native';
+import LoginScreen from "./App/Screen/LoginScreen/LoginScreen";
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, {useCallback} from "react";
+import {ClerkProvider, SignedIn, SignedOut} from "@clerk/clerk-expo";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded, fontError] = useFonts({
+        'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+        'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+        'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
+    return (
+        <ClerkProvider publishableKey={'pk_test_Y2VydGFpbi1ibG93ZmlzaC04MC5jbGVyay5hY2NvdW50cy5kZXYk'}>
+            <View style={styles.container} onLayout={onLayoutRootView}>
+                <SignedIn>
+                    <Text>You are Signed in</Text>
+                </SignedIn>
+                <SignedOut>
+                    <LoginScreen/>
+                </SignedOut>
+                <StatusBar style="auto"/>
+            </View>
+        </ClerkProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 25,
+    },
 });
